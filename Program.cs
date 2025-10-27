317e7c7e8296289cc70d5326b86a36a27bef0c73
JBelthoff <jb@johnbelthoff.com>
2025-10-27 06:47:30 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Running; 
+
+namespace PokerBenchmarks
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            // BenchmarkDotNet
+            BenchmarkRunner.Run<FinalRiverBench>();
+
+            // C++  Compare
+            //CPlusPlusBench.Run();
+            
+            // Full 9 player table
+            //Op9Bench.Run(10_000_000);
+
+        }
+    }
+}
